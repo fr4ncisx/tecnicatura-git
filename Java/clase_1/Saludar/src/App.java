711c2ffb662be50247279bd9bb184566994c7ab9
fr4ncisx <fr4ncisx@gmail.com>
2025-05-07 23:10:09 -0300

    Se agrega la clase 1 de java

---
+++ Java/clase_1/Saludar/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        saludar("Hola!!");
+    }
+    private static void saludar(String saludo){
+        System.out.println(saludo);
+    }
+}
