456109f5d4909ceaac54b688f96636c8a9b2919c
fr4ncisx <fr4ncisx@gmail.com>
2025-06-20 18:37:16 -0300

    se agrega clase 9 de javascript

---
+++ JavaScript/clase_9/ejercicio_1.js
@@ -0,0 +1,8 @@
+// Ejercicio: Si el padre puede asistir al juego de su hijo
+let vacaciones = true;
+let descanso = false;
+if(vacaciones || descanso){
+    console.log("Puede asistir al juego de su hijo");
+} else {
+    console.log("No puede asistir al juego de su hijo");
+}
