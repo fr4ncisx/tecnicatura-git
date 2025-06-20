175ac12c15e6b2646f3718aca545d6db7ea02dab
fr4ncisx <fr4ncisx@gmail.com>
2025-06-20 18:37:08 -0300

    se agrega clase 8 de javascript

---
+++ JavaScript/clase_8/ejercicio_1.js
@@ -0,0 +1,9 @@
+// Ejercicio: dentro de un rango
+let rango = 5;
+let min = 0
+let max = 10;
+if(rango >= min && rango <= max){
+    console.log('Dentro del rango');
+} else {
+    console.log('Fuera del rango');
+}
